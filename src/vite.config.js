e69ec6704a0b78c49665ac8dcda8184ecdf52d7a
takuya178 <takuya.u1005@gmail.com>
2023-06-03 17:46:40 +0900

    add first-commit

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    },
     plugins: [
+        react(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.jsx',
+            ],
             refresh: true,
         }),
     ],
