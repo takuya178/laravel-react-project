d7048b18af3aea31dc55731125d1c5cd8fc9cce8
takuya178 <takuya.u1005@gmail.com>
2023-06-06 08:25:31 +0900

    add table

---
+++ src/database/migrations/2023_06_05_231056_create_categories_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('primary_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('sort_order');
+            $table->timestamps();
+        });
+
+        Schema::create('secondary_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('sort_order');
+            // 外部キー制約
+            $table->foreignId('primary_category_id')
+            ->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('primary_categories');
+        Schema::dropIfExists('secondary_categories');
+    }
+};
